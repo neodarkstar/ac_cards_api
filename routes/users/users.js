076ca4094b17c1b026b3c334c5eda9dc68ba44cf
John Llombart <johnllombart@gmail.com>
2015-10-03 11:03:11 -0400

    Added qty validation

--- routes/users/users.js
+++ routes/users/users.js
@@ -90,31 +90,33 @@ router
     db.then(function(_db){
       var collection = _db.collection('users');
       var cursor = collection.find({ id: parseInt(req.params.id) });
+      var qty = parseInt(req.body.qty);
 
-      cursor.count(function(err, count){
-        if(err)
-          console.log('error', err);
+      if(qty < 0){
+        res.status(400).send('Qty must be positive');
+      } else {
+        cursor.count(function(err, count){
+          if(err)
+            console.log('error', err);
 
-        if(count == 0)
-          res.sendStatus(404);
-        else {
-          cursor.toArray(function(err, documents){
-            var document = documents[0];
+          if(count == 0)
+            res.sendStatus(404);
+          else {
+            cursor.toArray(function(err, documents){
+              var document = documents[0];
 
-            var index =_.findIndex(document.cards, { number: req.params.cardId });
+              var index =_.findIndex(document.cards, { number: req.params.cardId });
 
-            document.cards[index].qty = parseInt(parseInt(req.body.qty));
+              document.cards[index].qty = parseInt(parseInt(req.body.qty));
 
-            collection.update({ id: parseInt(req.params.id)}, document);
+              collection.update({ id: parseInt(req.params.id)}, document);
 
-            res.json(document);
+              res.json(document);
 
-          });
-        }
-
-      })
-
-
+            });
+          }
+        })
+      }
     });
   });
 
