42b5ee4d9fae33a20d600905e17dbbc0a91b5f92
John Llombart <johnllombart@gmail.com>
2015-10-08 22:32:40 -0400

    Added shipit.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var Q = require('q');
-var routes = require('./routes/routes.js');
+var routes = require('./routes');
 var logger = require('./logger.js');
 
 app.use(bodyParser.urlencoded({ extended: true }));
