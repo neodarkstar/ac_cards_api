95181141e7c792b6b6cbaaffc66c007ed8942409
John Llombart <johnllombart@gmail.com>
2015-10-09 21:17:24 -0400

    Added CORS support in global router

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,16 @@ var login = require('./login');
 var cards = require('./cards');
 var users = require('./users');
 
-router.use('/login', login);
-
-
 router.use(function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods","POST, GET, OPTIONS, PUT");
+  next();
+})
+
+router.use('/login', login);
+
+router.use(function(req, res, next){
 
   var requestInfo = {
     url: req.originalUrl,
